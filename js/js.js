ce90436f3b72955daf1efd8dd886ddaf0bc4b288
Dungchv13 <Dungutc13@gmail.com>
2020-09-11 13:52:05 +0700

    add doyou_loveme

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function co() {
+    alert(" <3 ");
+}
+function khong(){
+
+var x = Math.round(Math.random() * innerHeight );
+var y = Math.round(Math.random() * innerWidth);
+
+
+document.getElementById("no"). style. left = y + 'px';
+document.getElementById("no"). style. top = x + 'px';
+
+}
